9b50147ea924d6bfc2143e6bb4875c18fd7959a4
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-16 00:55:04 +0530

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import Home from '@/modules/home/home'
+// import Home from '@/modules/home/features/home'
 
-function page() {
+// function page() {
+//   return (
+//     <div>
+//       <Home/>
+//     </div>
+//   )
+// }
+
+// export default page
+import Home from "@/modules/home/home";
+
+const Page = () => {
   return (
     <div>
-      <Home/>
+      <Home /> 
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
